8ed932732981307ee31c6ad8719e79479f77bac0
James Clarke <james.clarke@tcu.edu>
2023-09-11 15:34:35 -0500

    feat: Student class added

---
+++ src/main/java/dev/clarke/rest/models/Student.java
@@ -0,0 +1,51 @@
+package dev.clarke.rest.models;
+
+public class Student {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String major;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+}
